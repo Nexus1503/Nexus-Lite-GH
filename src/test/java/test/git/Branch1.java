53094eaf4b56ebb98e8ce89e0bd68dd2fb1af2ef
sandip thopate <sandipthopate23@gmail.com>
2024-06-13 01:59:09 +0530

    commit 2.

--- src/test/java/test/git/Branch1.java
+++ src/test/java/test/git/Branch1.java
@@ -7,6 +7,8 @@ public class Branch1 {
 		
 		System.out.println("This is class Branch1");
 		System.out.println("This is class Branch1, the text added in branch2.");
+		System.out.println("abcd");
+		System.out.println("xyz.");
 		System.out.print("changes from branch b2");
 	}
 
