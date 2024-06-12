8172c3a904e1c1b00f8bfe9a19b71aa4047e5e52
sandip thopate <sandipthopate23@gmail.com>
2024-06-12 23:36:23 +0530

    Add class Branch1.

---
+++ src/test/java/test/git/Branch1.java
@@ -0,0 +1,12 @@
+package test.git;
+
+public class Branch1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is class Branch1");
+
+	}
+
+}
