0987f5e786a843f96030c56db49f8ed83fbf013e
sandip thopate <sandipthopate23@gmail.com>
2024-07-27 03:20:57 +0530

    Fix some more errors.

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
@@ -15,35 +16,41 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import utilities.ReportLibrary;
 
 public class BaseTest {
-	
+
 	public WebDriver driver;
-	
+
 	public ExtentReports extentReport = ReportLibrary.getReporter();
 	public ExtentTest extentTest;
 	public String methodName;
-	
+
 	@BeforeMethod
 	protected void setUp() {
 		
 		
 	
-		WebDriverManager.chromedriver().setup();
+		WebDriverManager.chromedriver().setup();	
 		
-		driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.manage().deleteAllCookies();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(40));
+		ChromeOptions options = new  ChromeOptions();
+				options.addArguments("--ignore-certificate-errors");
+				options.addArguments("--disable-dev-shm-usage");
+				
+				driver = new ChromeDriver(options);
+				driver.manage().window().maximize();
+				driver.manage().deleteAllCookies();
+				driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(40));
+			
 		
 	}
-	
+
 	@AfterMethod
 	protected void teadDown() {
-		
+
 		driver.quit();
 	}
+
 	@AfterSuite
 	public void endSuite() {
-	//	extentReport.flush();
+		// extentReport.flush();
 	}
-	
+
 }
